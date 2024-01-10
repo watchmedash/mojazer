78d7e6432e8a4269b821acf931ef0073d9a562c3
watchmedash <mojazer2017@gmail.com>
2024-01-10 10:56:01 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
   const images = document.querySelectorAll('.image-container img');
+  const container = document.querySelector('.image-container');
   let currentImageIndex = 0;
 
   function handleScroll() {
     const scrollPosition = window.scrollY;
     const newImageIndex = Math.floor(scrollPosition / window.innerHeight);
 
-    if (newImageIndex !== currentImageIndex) {
-      images[currentImageIndex].style.opacity = 0;
-      images[newImageIndex].style.opacity = 1;
-      currentImageIndex = newImageIndex;
-    }
+    const translateY = -newImageIndex * 100;
+    container.style.transform = `translateY(${translateY}vh)`;
   }
 
   window.addEventListener('scroll', handleScroll);
-
-  // Trigger initial check to set the correct image based on initial scroll position
-  handleScroll();
 });
